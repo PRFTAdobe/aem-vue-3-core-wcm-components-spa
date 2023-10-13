5693616bd02e77ef8c6d8bc3b9e5c860f1f7971b
Shannon Sumner <108552773+shannonsumner@users.noreply.github.com>
2023-10-13 10:10:05 -0400

    Adding Experience Fragment

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 export {
   AccordionEditConfig,
   ContainerEditConfig,
+  ExperienceFragmentEditConfig,
 } from '@/components/CoreEditConfigs';
 export { default as CoreContainer } from '@/components/CoreContainer.vue';
 export { default as CoreAccordion } from '@/components/CoreAccordion.vue';
+export { default as CoreExperienceFragment } from '@/components/CoreExperienceFragment.vue';
